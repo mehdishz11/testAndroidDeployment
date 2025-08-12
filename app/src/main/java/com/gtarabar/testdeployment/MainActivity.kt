0f92db13f8f8e03b68b6cb8e439468eb7e2bd0fa
Mehdi Shojaeian <Mehdishz12@gmail.com>
2025-08-12 17:55:36 +0330

    test commit

--- app/src/main/java/com/gtarabar/testdeployment/MainActivity.kt
+++ app/src/main/java/com/gtarabar/testdeployment/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        //test
         setContent {
             TestDeploymentTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
